894ac3a93999442328b31aeb6df6df09438afd7b
mammo0 <marc.ammon@hotmail.de>
2020-05-11 21:48:23 +0200

    do the setting or deleting of a classproperty always on the class
    -> if changing the property on an instance, the class property get
    changed, not the instance one

--- simple_classproperty/__init__.py
+++ simple_classproperty/__init__.py
@@ -9,22 +9,45 @@ class classproperty(property):
 
     Example:
         @classproperty
-        def prop(self):
+        def prop(cls):
             return "value"
     """
-    def __get__(self, instance, cls) -> object:
+    def __get__(self, _, cls) -> object:
         """
         This method gets called when a property value is requested.
-        @param instance: The instance with the property.
         @param cls: The class type of the above instance.
         @return: The value of the property.
         """
-        # if an instance is provided, use it
-        if instance is not None:
-            return super(classproperty, self).__get__(instance)
-        # otherwise the class
+        # apply the __get__ on the class
+        return super(classproperty, self).__get__(cls)
+
+    def __set__(self, cls_or_instance, value: object) -> None:
+        """
+        This method gets called when a property value should be set.
+        @param cls_or_instance: The class or instance of which the property should be changed.
+        @param value: The new value.
+        """
+        # call this method only on the class, not the instance
+        super(classproperty, self).__set__(self.__get_class(cls_or_instance), value)
+
+    def __delete__(self, cls_or_instance) -> None:
+        """
+        This method gets called when a property should be deleted.
+        @param cls_or_instance: The class or instance of which the property should be deleted.
+        """
+        # call this method only on the class, not the instance
+        super(classproperty, self).__delete__(self.__get_class(cls_or_instance))
+
+    def __get_class(self, cls_or_instance) -> type:
+        """
+        Get the class of an object if one is provided.
+        @param cls_or_instance: Either an object or a class.
+        @return: The class of the object or just the class again.
+        """
+        if isinstance(cls_or_instance, type):
+            return cls_or_instance
         else:
-            return super(classproperty, self).__get__(cls)
+            return type(cls_or_instance)
 
 
 class ClassPropertyMeta(type):
